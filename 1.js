a87247e48928dd05cde2d166beb25540ab8005f3
sergocode <69472815+sergocode@users.noreply.github.com>
2020-10-30 23:49:58 +0300

    +
    +

---
+++ 1.js
@@ -0,0 +1,29 @@
+var count = Math.ceil((Math.random()*101-1)*3);
+var min_count = 4;
+
+function generate_candles() {
+	var total_count = min_count + count;
+	for (var i = 1; i <= (min_count + count); i++) {
+	     var random_animation = Math.ceil((Math.random()*101-1)/4+3);
+	     var random_flying = Math.ceil((Math.random()*101-1)/4+10);
+	       		var random_margin_horizontal = Math.ceil((Math.random()*101-1)/1-51);
+	       		var random_margin_vertical = Math.ceil((Math.random()*101-1)/2-51);
+	       		var candlesblock = document.getElementById("candlesblock");
+	         	let candle = document.createElement('div');
+		    	candle.id = "id"+i;
+				candle.className = "candle";
+				candlesblock.append(candle);
+				document.getElementById(candle.id).style.animation="show " + random_animation + "s ease 0s 1 normal forwards running" + ", flying " + random_flying + "s infinite";
+				document.getElementById(candle.id).style.margin= random_margin_vertical+"px " + random_margin_horizontal+"px";
+				// document.getElementById(candle.id).style.left= "50px";
+	}
+} 
+
+
+
+function flying_animation() {
+	document.getElementById(candle.id).style.border= "solid 2px #fff"
+		
+}
+
+generate_candles()
