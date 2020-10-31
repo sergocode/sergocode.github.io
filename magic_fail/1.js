1248fbd8db24a8297bfc551539840c34604ec851
sergocode <69472815+sergocode@users.noreply.github.com>
2020-10-31 17:09:04 +0300

    +

--- magic_fail/1.js
+++ magic_fail/1.js
@@ -1,4 +1,7 @@
-var count = Math.ceil((Math.random()*101-1)*3);
+var count = Math.ceil((Math.random()*101-1)-100);
+	if (count < 30) {
+		count = 30;
+	}
 var min_count = 4;
 
 function generate_candles() {
@@ -6,7 +9,7 @@ function generate_candles() {
 	for (var i = 1; i <= (min_count + count); i++) {
 	     var random_animation = Math.ceil((Math.random()*101-1)/4+3);
 	     var random_flying = Math.ceil((Math.random()*101-1)/4+10);
-	       		var random_margin_horizontal = Math.ceil((Math.random()*101-1)/1-51);
+	       		var random_margin_horizontal = Math.ceil((Math.random()*101-1)/1-21);
 	       		var random_margin_vertical = Math.ceil((Math.random()*101-1)/2-51);
 	       		var candlesblock = document.getElementById("candlesblock");
 	         	let candle = document.createElement('div');
@@ -15,7 +18,6 @@ function generate_candles() {
 				candlesblock.append(candle);
 				document.getElementById(candle.id).style.animation="show " + random_animation + "s ease 0s 1 normal forwards running" + ", flying " + random_flying + "s infinite";
 				document.getElementById(candle.id).style.margin= random_margin_vertical+"px " + random_margin_horizontal+"px";
-				// document.getElementById(candle.id).style.left= "50px";
 	}
 } 
 
